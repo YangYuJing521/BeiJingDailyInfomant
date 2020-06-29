bcca4c1ca2fda7a138ba648424fc24f8d7c5d5b3
silverBullet <ios_yangyujing@163.com>
2020-06-29 12:19:31 +0800

    补充点击效果

--- Example/BeiJingDailyInfomant/BJH(北京号)/viewController/BDBJHListController.m
+++ Example/BeiJingDailyInfomant/BJH(北京号)/viewController/BDBJHListController.m
@@ -37,6 +37,8 @@
     return cell;
 }
 
-
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
 
 @end
